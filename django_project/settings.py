57c1d42ed756060cd671224fb49fb6c72ecd9546
Jerico <jericowaller@gmail.com>
2022-09-15 12:01:24 -0600

    heroku commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%&6c=!5)%dde)tkayj9uwbwxkwka!ubuz9up7t7-carw&76pz3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
